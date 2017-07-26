3eb9c65499ead6315a88ee772625e1116d6afa0d
d1ma-angel <dmitriy.angelovskiy@gmail.com>
2017-07-26 16:35:22 +0300

    Save clients from Visit page to database

--- app.rb
+++ app.rb
@@ -24,12 +24,35 @@ get '/visit' do
     erb :visit
 end
 
-get '/visit' do
+post '/visit' do
     @username = params[:username]
     @phone = params[:phone]
     @datetime = params[:datetime]
     @barber = params[:barber]
     @color = params[:color]
 
+    hh = {  :username => 'Введите имя',
+            :phone => 'Введите телефон',
+            :datetime => 'Введите дату и время' }
+
+    @error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+    if @error != ''
+        return erb :visit
+    end
+
+    user = Client.new do |u|
+      u.name = @username
+      u.phone = @phone
+      u.datestamp = @datetime
+      u.barber = @barber
+      u.color = @color
+    end
+    user.save
+
     erb "<h2>Спасибо, вы записались!</h2>"
+end
+
+get '/contacts' do
+    erb :contacts
 end
