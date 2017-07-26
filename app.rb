73270dc3728172a03d780a6909ccc1b3807f590f
d1ma-angel <dmitriy.angelovskiy@gmail.com>
2017-07-26 23:15:23 +0300

    Display error text

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-    validates :name, { presence: true }
-    validates :phone, { presence: true }
-    validates :datestamp, { presence: true }
-    validates :color, { presence: true }
+    validates :name, presence: true
+    validates :phone, presence: true
+    validates :datestamp, presence: true
+    validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
@@ -31,9 +31,12 @@ end
 post '/visit' do
 
     c = Client.new params[:client]
-    c.save
-
-    erb "<h2>Спасибо, вы записались!</h2>"
+    if c.save
+        erb "<h2>Спасибо, вы записались!</h2>"
+    else
+        @error = c.errors.full_messages.first
+        erb :visit
+    end
 end
 
 get '/contacts' do
